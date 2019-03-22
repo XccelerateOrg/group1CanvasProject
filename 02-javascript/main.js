a9cfbfafea66c3f8b0887deca32a761cde6a6ec2
Willy HUI <willyhui94@gmail.com>
2019-03-22 15:49:35 +0800

    update html layout

--- 02-javascript/main.js
+++ 02-javascript/main.js
@@ -2,17 +2,15 @@ function chnageElementSize() {
     let screenWidth = $(window).width();
     let screenHeight = $(window).height() - 10;
 
-    // $('#container').css('height', screenHeight * 0.8);
-    // $('#bottomMenu').css('height', screenHeight * 0.2);
+    $('#sideMenu').css('width', Math.floor(screenWidth * 0.15));
+    $('#canvas').attr('width', Math.floor(screenWidth * 0.85));
 
-    console.log(`screenWidth ${screenWidth}`);
-    console.log(`screenHeight ${screenHeight}`);
-
-    $('#sideMenu').css('width', Math.floor(screenWidth * 0.13));
-    $('#canvas').attr('width', Math.floor(screenWidth * 0.87));
+    let sideMenuRowWidth = $('#sideMenu').width();
+    console.log(sideMenuRowWidth)
+    $('.sideMenuBtn').css('width', sideMenuRowWidth * 0.45);
+    $('.sideMenuBtn').css('height', sideMenuRowWidth * 0.45);
 
     let sideMenuHeight = $('#sideMenu').outerHeight();
-    console.log(`sideMenuHeight ${sideMenuHeight}`)
     $('#canvas').attr('height', sideMenuHeight);
 
     $('#sideMenu').css('height', Math.floor(screenHeight * 0.8));
