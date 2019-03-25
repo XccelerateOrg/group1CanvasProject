d1a59a6392be0a259b9aa53f3e8f53921d899256
Willy HUI <willyhui94@gmail.com>
2019-03-25 15:24:15 +0800

    add globalStyle.js

---
+++ TEST_ONLY/globalStyle.js
@@ -0,0 +1,25 @@
+class GlobalStyle {
+    constructor() {
+        // Line Style
+        this.lineThickness = 10;            // ctx.lineWidth = self.globalStyle.lineThickness   // the thickness of lines
+        this.lineEndShape = "round";        // ctx.lineCap = self.globalSrtle.lineEndShape      // the shape of ending point on the line "round圓角" "butt短方角" "square長方角"
+        this.lineJoinShape = "round";       // ctx.lineJoin = self.globalStyle.lineJoinShape    // the shape used to join to line "round圓角" "miter尖角" "bevel平角"
+        
+        // Text Style
+        this.fontSize = 10;                 // ctx.font = `${self.globalStyle.fontSize}px ${self.globalStyle.fontStyle}`
+        this.fontStyle = "sans-serif";      // ctx.font = `${self.globalStyle.fontSize}px ${self.globalStyle.fontStyle}`
+        this.textAlignment = "start"        // ctx.textAlign  = self.globalStyle.textAlignment      // "start" "end" "left" "right" "center"
+        this.textBaseline = "alphabetic"    // ctx.textBaseline  = self.globalStyle.textBaseline    // "top", "middle", "bottom", "hanging", "alphabetic", "ideographic"
+
+        // Fill and Stroke Style
+        this.fillColor = "#550088";         // ctx.fillStyle = self.globalStyle.fillColor     // shape inside color
+        this.lineColor = "#558888";         // ctx.strokeStyle = self.globalSytle.lineColor   // border color
+
+        // Shodows
+        this.shadowColor = "#ffffff";       // ctx.shadowColor = self.globalSyle.shadowColor
+        this.shadowBlurLevel = 0;           // ctx.shadowBlur = self.globalStyle.shadowBlurLevel
+        this.shadowExpandX = 0;             // ctx.shadowOffsetX = self.globalStyle.shadowExpandX
+        this.shadowExpandY = 0;             // ctx.shadowOffsetY = self.globalStyle.shadowExpandY
+
+    };
+}
