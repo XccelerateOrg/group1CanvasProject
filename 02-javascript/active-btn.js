cee55781d379237f5b6c0f406674d7f9849ff786
Mike Liu <mliu8m@gmail.com>
2019-03-26 17:07:55 +0800

    updated ui v2

--- 02-javascript/active-btn.js
+++ 02-javascript/active-btn.js
@@ -10,7 +10,6 @@ function removeFeature() {
     theClass.removeClass ('feature-active');
 };
 
-// --------------
 
 // style active, callback
 function activeStyle(theId) {
@@ -21,6 +20,49 @@ function removeStyle() {
     let theClass = $('.style-box');
     theClass.removeClass ('style-active');
 };
+
+
+
+function clickCurrentPicker(theId){
+    theId.on('click', function(){
+        $('.current-color').css({"z-index" : "0"});
+        theId.css({"z-index" : "10"});
+    });
+}
+
+// function indexToZero(){
+
+// }
+
+
+//style - change current color
+// function changeCurrentColor(){
+//     if ($('color-a').css({"z-index" : 1} == true)){
+//         $('#color-a').css("background-color", pick);
+//     }
+
+//     // $('color-b').on('click' , function(){
+//     //     $('color-b').addClass('higher-zindex');
+//     // })
+// };
+
+// changeCurrentColor();
+
+// // style - pick color, callback
+
+// // function pickColor() {
+// //     if ($('#color-a').css({"z-index" : 1} == true){
+// //         $('#color-a').css("background-color", pick);
+// //     }
+// // }
+
+// // style - color onclick, callback
+// function pick (theId) {
+//     theId.on('click' , function(){
+//         theId.css("background-color");
+//     })
+// };
+
 
 
 // --------------
@@ -85,6 +127,79 @@ $('#f-11').on('click', function () {
 
 // --------------
 
+// current color picker - styles
+$('#color-a').on('click', function () {
+    clickCurrentPicker($('#color-a'));
+});
+
+$('#color-b').on('click', function () {
+    clickCurrentPicker($('#color-b'));
+});
+
+// --------------
+
+// color onclick
+$('#picker-1').on('click', function () {
+    
+    pick($('#picker-1'));
+});
+
+$('#picker-2').on('click', function () {
+    
+    pick($('#picker-2'));
+});
+
+$('#picker-3').on('click', function () {
+    
+    pick($('#picker-3'));
+});
+
+$('#picker-4').on('click', function () {
+    
+    pick($('#picker-4'));
+});
+
+$('#picker-5').on('click', function () {
+    
+    pick($('#picker-5'));
+});
+
+$('#picker-6').on('click', function () {
+    
+    pick($('#picker-6'));
+});
+
+$('#picker-7').on('click', function () {
+    
+    pick($('#picker-7'));
+});
+
+$('#picker-8').on('click', function () {
+    
+    pick($('#picker-8'));
+});
+
+// feature btn,sec 2 - icon
+$('#picker-9').on('click', function () {
+    
+    pick($('#picker-9'));
+});
+
+$('#picker-10').on('click', function () {
+    
+    pick($('#picker-10'));
+});
+
+$('#picker-11').on('click', function () {
+    
+    pick($('#picker-11'));
+});
+
+
+
+
+// --------------
+
 // style btn - styles
 $('#s-1').on('click', function () {
     removeStyle();
@@ -111,6 +226,7 @@ $('#s-5').on('click', function () {
     activeStyle($('#s-5'));
 });
 
+// back to default setting
 $('#default').on('click', function () {
     removeFeature();
     removeStyle();
