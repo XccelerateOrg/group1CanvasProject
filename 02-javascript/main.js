2d6547f55ce40f55d7ab5f3859eeb17261abf22d
Willy HUI <willyhui94@gmail.com>
2019-03-26 11:14:56 +0800

    update willy_feature

--- 02-javascript/main.js
+++ 02-javascript/main.js
@@ -22,27 +22,21 @@ $(document).ready(function () {
     chnageWindowSize();
     $(window).resize(chnageWindowSize);
 });
-const canvasDraft = $('#canvas-draft')[0];
-const ctxDraft = canvasDraft.getContext('2d');
-const canvasReal = $('#canvas-real')[0];
-const ctxReal = canvasReal.getContext('2d');
-let paint = undefined;
-let ctrl = false;
 
 
+// definition of DrawLine class
 class DrawLine{
     constructor() {
-        this.globalStyle = new GlobalStyle();
         this.drawing = false;
         self = this;
     };
     startPosition(e) {
-        if (e.which === 1 && ctrl) {
+        if (e.which === 1 && ctrlButton) {
             console.log(e.which);
             this.drawing = true;
-            ctxReal.lineWidth = self.globalStyle.lineThickness;
-            ctxReal.lineCap = self.globalStyle.lineEndShape;
-            ctxReal.strokeStyle = self.globalStyle.lineColor;
+            ctxReal.lineWidth = globalStyle.lineThickness;
+            ctxReal.lineCap = globalStyle.lineEndShape;
+            ctxReal.strokeStyle = globalStyle.lineColor;
             ctxReal.beginPath();
             ctxReal.moveTo(e.offsetX, e.offsetY);
         }
@@ -52,23 +46,132 @@ class DrawLine{
         ctxReal.stroke();
     }
     movePosition(e) {
-        if (this.drawing === true && ctrl === true) {
+        if (this.drawing === true && ctrlButton === true) {
             ctxReal.lineTo(e.offsetX, e.offsetY);           
             ctxReal.stroke();
         }
     }
 }
 
+// definition of DrawRectangle class
+class DrawRectangle {
+    constructor() {
+        this.drawing = false;
+        this.startPoint = [];
+        this.endPoint = [];
+        this.difference = [];
+        self = this;
+    };
+    startPosition(e) {
+        this.drawing = true;
+        ctxReal.lineWidth = globalStyle.lineThickness;
+        ctxReal.fillStyle = globalStyle.fillColor;
+        ctxDraft.lineWidth = globalStyle.lineThickness;
+        ctxDraft.fillStyle = globalStyle.fillColor;
+        self.startPoint[0] = e.offsetX;
+        self.startPoint[1] = e.offsetY;
+    }
+    endPosition(e) {
+        this.drawing = false;
+        ctxDraft.clearRect(0, 0, canvasDraft[0].width, canvasDraft[0].height);  
+        ctxReal.fillRect(self.startPoint[0], self.startPoint[1], self.difference[0], self.difference[1]);
+    }
+    movePosition(e) {
+        if (this.drawing === true) {
+            ctxDraft.clearRect(0, 0, canvasDraft[0].width, canvasDraft[0].height);        
+            self.difference[0] = e.offsetX - self.startPoint[0];
+            self.difference[1] = e.offsetY - self.startPoint[1];
+            ctxDraft.fillRect(self.startPoint[0], self.startPoint[1], self.difference[0], self.difference[1]);
+        }
+    }
+}
 
-paint = new DrawLine();
-console.log(paint);
-canvasDraft.addEventListener('mousedown', paint.startPosition);
-canvasDraft.addEventListener('mousemove', paint.movePosition);
-canvasDraft.addEventListener('mouseup', paint.endPosition);
+// definition of DrawCircle class
+class DrawCircle {
+    constructor() {
+        this.drawing = false;
+        this.firstPoint = [];
+        self = this;
+    };
+    startPosition(e) {
+        this.drawing = true;
+        ctxDraft.fillStyle = globalStyle.fillColor;
+        ctxDraft.beginPath();
+        self.firstPoint[0] = e.offsetX;
+        self.firstPoint[1] = e.offsetY;
+    }
+    endPosition(e) {
+        let sqX = Math.pow(e.offsetX - self.firstPoint[0], 2);
+        let sqY = Math.pow(e.offsetY - self.firstPoint[1], 2);
+        let radius = Math.sqrt(sqX + sqY);
+        console.log(radius);
+        ctxDraft.arc(self.firstPoint[0], self.firstPoint[1], radius, 0, 2 * Math.PI);
+        // ctx.stroke();
+        ctxDraft.fill();
+        this.drawing = false;
+    }
+    movePosition(e) {
+        return;
+    }
+}
+
+function removeHandler(canvas) {
+    canvas.off('mousedown');
+    canvas.off('mousemove');
+    canvas.off('mouseup');
+}
+
+function addHandler(canvas, paint) {
+    canvas.on('mousedown', paint.startPosition);
+    canvas.on('mousemove', paint.movePosition);
+    canvas.on('mouseup', paint.endPosition);
+}
+
+
+// program start at here!!!
+chnageWindowSize();
+const canvasDraft = $('#canvas-draft');
+const ctxDraft = canvasDraft[0].getContext('2d');
+const canvasReal = $('#canvas-real');
+const ctxReal = canvasReal[0].getContext('2d');
+const globalStyle = new GlobalStyle();
+let paint = undefined;
+let ctrlButton = false;
+
+
+paint = new DrawCircle();
+addHandler(canvasDraft, paint);
+
+// binding functionlity to button
+$('#freeStyle').click(function() {
+    removeHandler(canvasDraft);
+    paint = new DrawLine();
+    addHandler(canvasDraft, paint);
+    console.log(`freestyle`);
+    console.log(paint);
+})
+
+$('#rectangle').click(function() {
+    removeHandler(canvasDraft);
+    paint = new DrawRectangle();
+    addHandler(canvasDraft, paint);
+    console.log(`rectangle`);
+    console.log(paint);
+})
+
+$('#circle').click(function() {
+    removeHandler(canvasDraft);
+    paint = new DrawCircle();
+    addHandler(canvasDraft, paint);
+    console.log(`circle`);
+    console.log(paint);
+})
+
+// binding keybroad event
 document.addEventListener('keydown', function(e) {
     switch (e.which) {
         case 17:
-            ctrl = true;
+            ctrlButton = true;
             console.log(`ctrl = true`);
             break;
         default:
@@ -78,7 +181,7 @@ document.addEventListener('keydown', function(e) {
 document.addEventListener('keyup', function(e) {
     switch (e.which) {
         case 17:
-            ctrl = false;
+            ctrlButton = false;
             console.log(`ctrl = true`);
             break;
         default:
