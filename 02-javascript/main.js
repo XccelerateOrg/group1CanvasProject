b535811d52a8c85a0e53782b28f43f63fea12f1b
Mike Liu <mliu8m@gmail.com>
2019-03-26 12:19:21 +0800

    ui done, v0.1

--- 02-javascript/main.js
+++ 02-javascript/main.js
@@ -6,27 +6,30 @@ function chnageWindowSize() {
     let screenHeight = $(window).innerHeight;
 
     let windowWidth = $(window).width() - 49;
-    let windowHeight = $(window).height() - 90;
+    let windowHeight = $(window).height() - 72;
 
     //width
     $('.feature-wrapper').css('width', Math.floor(screenWidth * 0.1));
     $('.canvas').attr('width', Math.floor(windowWidth));
+    // console.log($('.canvas')[0].width);
+    
     $('.canvas').css('left', '45px');
     $('.style-wrapper').css('width', Math.floor(screenWidth));
 
 
     //height
-    $('.feature-wrapper').css('height', Math.floor(windowHeight) - 20);
+    $('.feature-wrapper').css('height', Math.floor(windowHeight) - 35);
     $('.canvas').css('position', 'fixed');
     $('.canvas').css('top', '24px');
     $('.feature-wrapper').css('margin-top', '24px');
 
     $('.canvas').attr('height', Math.floor(windowHeight)-10);
-    $('.style-wrapper').css('height', Math.floor(screenHeight));
+    $('.style-wrapper').css('height', Math.floor(screenHeight)-50);
 };
 
 $(document).ready(function () {
     chnageWindowSize();
     $(window).resize(chnageWindowSize);
 });
+// console.log($('.canvas')[0].width);
 
