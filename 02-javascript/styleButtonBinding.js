dd311e2058d79d2fee6b3822c4a7974593dde23b
Willy HUI <willyhui94@gmail.com>
2019-03-27 19:28:46 +0800

    update willy_feature

--- 02-javascript/styleButtonBinding.js
+++ 02-javascript/styleButtonBinding.js
@@ -1,3 +1,16 @@
+// change set line color or shape color
+$('#current-box').click(function() {
+    if (colorLineOrShape !== 1) {
+        colorLineOrShape = 1;
+    }
+    else {
+        colorLineOrShape = 2;
+    }
+    console.log(`colorLineOrShape ${colorLineOrShape}`);
+})
+
+// seting globalStyle.lineColor
+// seting globalStyle.fillColor
 $('#picker-1').click(function() {
     if(colorLineOrShape === 1) {
         globalStyle.lineColor = $(this).css("background-color");
@@ -118,13 +131,41 @@ $('#picker-10').click(function() {
     }
 })
 
+$('#lineThickness').click(function() {
+    globalStyle.lineThickness += 5;
+    if (globalStyle.lineThickness > 25) {
+        globalStyle.lineThickness = 5;
+    }
+    console.log(`lineThickness ${globalStyle.lineThickness}`);
+})
 
-$('#current-box').click(function() {
-    if (colorLineOrShape !== 1) {
-        colorLineOrShape = 1;
+$('#opacity').click(function() {
+    globalStyle.opacity -= 0.2;
+    if (globalStyle.opacity < 0.1) {
+        globalStyle.opacity = 1;
+    }
+    console.log(`opacity ${globalStyle.opacity}`);
+})
+
+$('#fontSize').click(function() {
+    globalStyle.fontSize += 5;
+    if (globalStyle.fontSize > 25) {
+        globalStyle.fontSize = 5;
+    }
+    console.log(`fontSize ${globalStyle.fontSize}`);
+})
+
+$('#alignment').click(function() {
+    if (globalStyle.textAlignment === 'left') {
+        globalStyle.textAlignment = "center";
+        console.log(`alignment ${globalStyle.textAlignment}`);
+    }
+    else if (globalStyle.textAlignment === 'center') {
+        globalStyle.textAlignment = "right";
+        console.log(`alignment ${globalStyle.textAlignment}`);
     }
     else {
-        colorLineOrShape = 2;
+        globalStyle._textAlignment = "left";
+        console.log(`alignment ${globalStyle.textAlignment}`);
     }
-    console.log(`colorLineOrShape ${colorLineOrShape}`);
 })
